75090f16ffc25fe1907998bc9dfb63e2d91b20a6
Peter Crotty <peter@crotty.co.uk>
2012-05-03 15:01:53 +0100

    Fix typo in test name.

--- src/test/java/com/playfish/CopyTest.java
+++ src/test/java/com/playfish/CopyTest.java
@@ -6,15 +6,16 @@ import org.junit.Test;
 public class CopyTest  {
 
     @Test
-	public void test_IfMemberABorrowsCopy_CopyIsOnLoadToMemberA()
-	{
-		Member memberA = new Member();
-		
-		Copy dvdCopy = new Copy();
-		
-		dvdCopy.borrow(memberA);
+    public void test_IfMemberABorrowsCopy_CopyIsOnLoanToMemberA()
+    {
+        Member memberA = new Member();
+        
+        Copy dvdCopy = new Copy();
+        
+        dvdCopy.borrow(memberA);
 
 
-		Assert.assertTrue(dvdCopy.onLoanTo == memberA);
-	}
+        Assert.assertTrue(dvdCopy.onLoanTo == memberA);
+    }
+
 }
