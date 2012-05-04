57e905a37942a92b911068ced0928f3b13209a9c
Stylianos Modes <stylianos.modes@playfish.com>
2012-05-04 11:35:14 +0100

    Move sendNewsletter test into an acceptance test file

--- src/test/java/com/playfish/LibraryUnitTest.java
+++ src/test/java/com/playfish/LibraryUnitTest.java
@@ -10,19 +10,6 @@ import org.junit.Test;
 
 public class LibraryUnitTest {
 
-	@Test
-    public void testSendNewsLetter_WhenHasNewTitle_Send(){
-		EmailClient emailClient = EasyMock.createMock(EmailClient.class);
-		Member member = new Member("a@gmail.com");
-		EasyMock.expect(emailClient.emailMember((String)EasyMock.anyObject(), (String)EasyMock.anyObject())).andReturn(true).atLeastOnce();
-		EasyMock.replay(emailClient);
-    	Library library = new Library();
-    	library.addMember(member);
-    	library.sendNewsLetter(emailClient);
-    	
-    	EasyMock.verify(emailClient);
-    }
-    
     @Test 
     public void testListNewTitles_WhenNewTitlesAreAvailable(){
     	Library library = new Library();
---
+++ src/test/java/com/playfish/SendNewsLetterAcceptanceTest.java
@@ -0,0 +1,28 @@
+package com.playfish;
+
+import java.util.Date;
+
+import org.easymock.EasyMock;
+import org.junit.Test;
+
+public class SendNewsLetterAcceptanceTest {
+    
+    @Test
+    public void testSendNewsLetter_WhenHasNewTitle_Send(){
+        EmailClient emailClient = EasyMock.createMock(EmailClient.class);
+        Member member = new Member("a@gmail.com");
+        EasyMock.expect(emailClient.emailMember((String)EasyMock.anyObject(), (String)EasyMock.anyObject())).andReturn(true).atLeastOnce();
+        EasyMock.replay(emailClient);
+        
+        Library library = new Library();
+        Date oldNewsletterDate = new Date(1336127334);
+        library.setLastNewsLetterDate(oldNewsletterDate);
+        
+        library.addMember(member);
+        library.submitDvd("testTitle", "testYear", "testDirector");
+        
+        library.sendNewsLetter(emailClient);
+        
+        EasyMock.verify(emailClient);
+    }
+}
