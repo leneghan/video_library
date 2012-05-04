a133095c2bdb5fbf728b2320120327b17dc2cf7a
Stuart Leneghan <email@leneghan.com>
2012-05-04 16:29:32 +0100

    Added a unit test for DvdViewPanel to borrow a dvd

---
+++ src/test/java/com/playfish/view/DvdViewPanelUnitTest.java
@@ -0,0 +1,11 @@
+package com.playfish.view;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: sleneghan
+ * Date: 04/05/2012
+ * Time: 16:15
+ * To change this template use File | Settings | File Templates.
+ */
+public class DvdViewPanelUnitTest {
+}
